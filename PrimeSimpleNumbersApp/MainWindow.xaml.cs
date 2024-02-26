8a2c2e06c0772bb6ca169aa4a96cf6d8259b444d
Envine21 <stepa@DESKTOP-OKT1C5V>
2024-02-27 00:56:16 +0300

    Добавьте файлы проекта.

---
+++ PrimeSimpleNumbersApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using PrimeSimpleNumbersApp;
+namespace SimpleNumbersApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FindPrimes_Click(object sender, RoutedEventArgs e)
+        {
+                int start = int.Parse(startTextBox.Text);
+                int end = int.Parse(endTextBox.Text);
+
+                if (start > end)
+                {
+                    MessageBox.Show("Начальное значение должно быть меньше конечного значения");
+                    return;
+                }
+
+                IEnumerable<int> primes = new PrimeNumberGenerator(start, end);
+
+                resultText.Text = $"Простые числа в диапазоне от {start} до {end} : {string.Join(" , ", primes)}";
+
+            }
+ 
+        }
+    }
+
+    
+
---
+++ PrimeSimpleNumbersApp/PrimeNumberGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeSimpleNumbersApp
+{
+    public class PrimeNumberGenerator : IEnumerable<int>
+    {
+        private readonly int start;
+        private readonly  int end;
+
+        public PrimeNumberGenerator(int start, int end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (isPrime(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool isPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
